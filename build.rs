8b6e89e67c78942c7e50e2c030628ee2fed51ed8
MALU <contact@andantissimo.jp>
2026-04-14 22:51:50 +0900

    add linker flags, fix warnings

--- build.rs
+++ build.rs
@@ -8,7 +8,13 @@ fn pkg_config(opt: &str, pkg: &str) -> Option<String> {
 
 fn main() {
     #[cfg(feature = "htpasswd")]
+    if let Some(l) = pkg_config("--libs-only-L", "openssl").filter(|s| !s.is_empty()) {
+        println!("cargo::rustc-link-arg={}", l)
+    }
+    #[cfg(feature = "htpasswd")]
     if pkg_config("--modversion", "openssl").is_some_and(|v| v.starts_with("1.0.")) {
-        println!("cargo:rustc-cfg={}", "openssl10")
+        println!("cargo::rustc-cfg={}", "openssl10")
+    } else {
+        println!("cargo::rustc-check-cfg=cfg({})", "openssl10")
     }
 }
